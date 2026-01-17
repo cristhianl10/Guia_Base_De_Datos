e6b72dbca72259301408772e364f749b166a7bfe
Cristhian Loor <cristhian.loor25@outlook.com>
2026-01-17 13:34:07 -0500

    second commit

--- 01_Reading/13_in.sql
+++ 01_Reading/13_in.sql
@@ -4,4 +4,5 @@ Lección 10.7: https://youtu.be/OuJerKzV5T0?t=8335
 */
 
 -- Ordena todos los datos de la tabla "users" con nombre igual a brais y sara
+--Se obtiene una búsqueda conjunta de varios valores de nombres en una sentencia (AND)
 SELECT * FROM users WHERE name IN ('brais', 'sara')
