7ca030dca9538feb688d24cb6ed7f3d4f47264ea
Cristhian Loor <cristhian.loor25@outlook.com>
2026-01-17 13:44:08 -0500

    third commit

--- 01_Reading/14_between.sql
+++ 01_Reading/14_between.sql
@@ -5,3 +5,6 @@ Lección 10.8: https://youtu.be/OuJerKzV5T0?t=8559
 
 -- Ordena todos los datos de la tabla "users" con edad comprendida entre 20 y 30
 SELECT * FROM users WHERE age BETWEEN 20 AND 30
+
+-- Obtiene el nombre e init_date de la tabla "users" con edad comprendida entre 20 y 30
+SELECT name, init_date FROM users WHERE age BETWEEN 20 AND 30
